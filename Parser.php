ae979a341b2dcaede921241d78c62cef4b848f89
Sergey Petrenko <spiatrenka@scnsoft.com>
2015-07-02 14:13:51 +0300

    travel-parser: get agency information and put it into csv

--- Parser.php
+++ Parser.php
@@ -1,28 +1,38 @@
 <?php
 
-require_once('Agency.php');
-
+/**
+ * Class Parser
+ */
 class Parser
 {
+    /**
+     * @param $url
+     * @return array
+     */
     public function parse($url)
     {
         libxml_use_internal_errors(true);
 
-        /*$agencyLinks = array();
+        $agencyLinks = array();
         for($i = 1; $i <= 10; $i++) {
             $innerUrl = $url . "?p=$i";
             $agencyLinks = $this->getAgencyLinks($innerUrl, $agencyLinks);
-        }*/
+        }
 
         $main_url = explode('/', $url);
-        $this->getAgencyInfo('http://www.holiday.by/agencies/tisjacha');
-        die();
         $agencies = array();
         foreach($agencyLinks as $key => $val) {
             $agencies[] = $this->getAgencyInfo('http://' . $main_url[2] . $val);
         }
+
+        return $agencies;
     }
 
+    /**
+     * @param $url
+     * @param array $resultLinks
+     * @return array
+     */
     private function getAgencyLinks($url, $resultLinks = array())
     {
         $doc = new DOMDocument();
@@ -37,35 +47,108 @@ class Parser
         return $resultLinks;
     }
 
+    /**
+     * @param $url
+     * @return Agency
+     */
     private function getAgencyInfo($url)
     {
+        echo $url . "<br/>";
         $doc = new DOMDocument();
         $doc->loadHTML(file_get_contents($url));
         $xpath = new DOMXpath($doc);
 
+        $agency = new Agency;
+
         $name = $xpath->query(".//h1");
-        echo str_replace('Турфирма ', '', $name->item(0)->nodeValue) . "<br/>";
+        $agency->name = str_replace('Турфирма ', '', $name->item(0)->nodeValue);
 
         $certificateNumber = $xpath->query('.//div[@class="cert"]/div[@class="no"]');
-        echo $certificateNumber->item(0)->nodeValue . "<br/>";
+        $agency->certificate = $certificateNumber->item(0)->nodeValue;
         $certificateSince = $xpath->query('.//div[@class="cert"]/div[@class="created_at"]');
-        echo str_replace('Выдан: ', '', $certificateSince->item(0)->nodeValue . "<br/>");
+        $agency->certificateSince = str_replace('Выдан: ', '', $certificateSince->item(0)->nodeValue);
         $certificateTo = $xpath->query('.//div[@class="cert"]/div[@class="expire_at"]');
-        echo str_replace('Действителен до: ', '', $certificateTo->item(0)->nodeValue . "<br/>");
+        $agency->certificateTo = str_replace('Действителен до: ', '', $certificateTo->item(0)->nodeValue);
 
         $city = $xpath->query('.//div[@class="opinions-block"]/following::p');
-        echo $city->item(0)->nodeValue . "<br/>";
         $address = $xpath->query(".//u[1]");
-        echo $address->item(0)->nodeValue . "<br/>";
+        $agency->address = $city->item(0)->nodeValue . ' ' . $address->item(0)->nodeValue;
 
         $website = $xpath->query('(.//div[@class="info-section"][1]/table/tr/td/p/a[@href="javascript://"])[2]');
-        echo $website->item(0)->nodeValue . "<br/>";
+        $agency->website = $website->item(0)->nodeValue;
 
         $worktime = $xpath->query('.//div[@class="info-section"][1]/table/tr/td/p[last()]');
-        echo str_replace('Время работы: ', '', $worktime->item(0)->nodeValue . "<br/>");
+        $agency->workTime = str_replace('Время работы: ', '', $worktime->item(0)->nodeValue);
 
+        $phonesWork = array();
+        $phones = $xpath->query('.//span[@title="Городской"]');
+        foreach ($phones as $val) {
+            $phonesWork[] = $val->nodeValue;
+        }
+        $agency->phonesWork = implode("\r\n", $phonesWork);
+
+        $phonesVelcom = array();
         $phones = $xpath->query('.//span[@title="Velcom"]');
+        foreach ($phones as $val) {
+            $phonesArray[] = $val->nodeValue;
+        }
+        $agency->phonesVelcom = implode("\r\n", $phonesVelcom);
 
-        die();
+        $phonesMts = array();
+        $phones = $xpath->query('.//span[@title="MTC"]');
+        foreach ($phones as $val) {
+            $phonesMts[] = $val->nodeValue;
+        }
+        $agency->phonesMts = implode("\r\n", $phonesMts);
+
+        $phonesLife = array();
+        $phones = $xpath->query('.//span[@title="life :)"]');
+        foreach ($phones as $val) {
+            $phonesLife[] = $val->nodeValue;
+        }
+        $agency->phonesLife = implode("\r\n", $phonesLife);
+
+        $icqArray = array();
+        $icq = $xpath->query('.//img[@title="ICQ"]/parent::p');
+        foreach ($icq as $val) {
+            $icqArray[] = $val->nodeValue;
+        }
+        $agency->icq = implode("\r\n", $icqArray);
+
+        $skypeArray = array();
+        $skype = $xpath->query('.//img[@title="Skype"]/parent::p');
+        foreach ($skype as $val) {
+            $skypeArray[] = $val->nodeValue;
+        }
+        $agency->skype = implode("\r\n", $skypeArray);
+
+        $needle = 'УНП';
+        $text = $xpath->query('.//div[@class="text-block text"]');
+        $agency->unp = substr($text->item(0)->nodeValue, strpos($text->item(0)->nodeValue, $needle) + strlen($needle));
+
+        $agency->link = $url;
+
+        return $agency;
     }
+}
+
+/**
+ * Class Agency
+ */
+class Agency {
+    public $name;
+    public $certificate;
+    public $certificateSince;
+    public $certificateTo;
+    public $address;
+    public $website;
+    public $phonesWork;
+    public $phonesVelcom;
+    public $phonesMts;
+    public $phonesLife;
+    public $workTime;
+    public $icq;
+    public $skype;
+    public $unp;
+    public $link;
 }
--- start.php
+++ start.php
@@ -1,26 +1,16 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+set_time_limit(0);
 
 require_once('Parser.php');
-//require_once('Nokogiri.php');
-//require_once('Encoding.php');
-//use \ForceUTF8\Encoding;
 $parser = new Parser();
+$agencies = array();
 
-$parser->parse('http://www.holiday.by/agencies/');
-//$parser->parse('http://www.w3.org/');
+$agencies = $parser->parse('http://www.holiday.by/agencies/');
+var_dump(count($agencies));
 
-//$html = file_get_contents('http://www.holiday.by/agencies/');
-//$html = file_get_contents('http://www.holiday.by/agencies/');
-//$html_utf8 = mb_convert_encoding($html, "utf-8", "windows-1251");
-//$saw = new nokogiri($html);
-//
-//var_dump($saw);
-//
-//$result = $saw->get('a.title')->toArray();
-//var_dump($result);
-
-//foreach($result as $key => $val) {
-//    var_dump(Encoding::toUTF8($val['#text']));
-//}
+$fp = fopen('/tmp/parsing.csv', 'w');
+foreach($agencies as $val){
+    fputcsv($fp, (array)$val);
+}
